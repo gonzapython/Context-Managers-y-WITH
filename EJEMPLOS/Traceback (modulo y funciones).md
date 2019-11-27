7e2d4d60732500394f5add279cc620f82ac5d585
gonzapython <56170708+gonzapython@users.noreply.github.com>
2019-11-27 17:01:19 +0100

    Traceback y más.....

---
+++ EJEMPLOS/Traceback (modulo y funciones).md
@@ -0,0 +1,23 @@
+
+traceback- Imprimir o recuperar un seguimiento de pila 
+------------------------------------------------------
+
+Código fuente: Lib / traceback.py
+
+Este módulo proporciona una interfaz estándar para extraer, formatear e imprimir rastros 
+de pila de programas Python.
+
+El módulo define las siguientes funciones:
+
+    `traceback.print_tb( tb , límite = Ninguno , archivo = Ninguno )`
+
+    `traceback.print_exception( etype , value , tb , limit = None , file = None , chain = True )`
+
+    `traceback.print_exc( límite = Ninguno , archivo = Ninguno , cadena = Verdadero )`
+
+    `traceback.extract_tb( tb , límite = Ninguno )`
+
+    `traceback.format_exc( límite = Ninguno , cadena = Verdadero )`
+
+    `traceback.format_tb( tb , límite = Ninguno )`
+
--- EJEMPLOS/tracebk.py
+++ EJEMPLOS/tracebk.py
@@ -12,32 +12,47 @@ try:
 except IndexError:
     exc_type, exc_value, exc_traceback = sys.exc_info()
 
-    print("*** print_tb:")
+    print("                    ")
+    print("*** print_traceback:")
+    print("--------------------")
     traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
 
+    print("                    ")
     print("*** print_exception:")
+    print("--------------------")
     # exc_type below is ignored on 3.5 and later
     traceback.print_exception(exc_type, exc_value, exc_traceback,
                               limit=2, file=sys.stdout)
 
+    print("              ")
     print("*** print_exc:")
+    print("--------------")
     traceback.print_exc(limit=2, file=sys.stdout)
 
+    print("                                    ")
     print("*** format_exc, first and last line:")
+    print("------------------------------------")
     formatted_lines = traceback.format_exc().splitlines()
     print(formatted_lines[0])
     print(formatted_lines[-1])
 
+    print("                     ")
     print("*** format_exception:")
+    print("---------------------")
     # exc_type below is ignored on 3.5 and later
     print(repr(traceback.format_exception(exc_type, exc_value,
                                           exc_traceback)))
 
-    print("*** extract_tb:")
+    print("                      ")
+    print("*** extract_traceback:")
+    print("----------------------")
     print(repr(traceback.extract_tb(exc_traceback)))
 
-    print("*** format_tb:")
+    print("                     ")
+    print("*** format_traceback:")
+    print("---------------------")
     print(repr(traceback.format_tb(exc_traceback)))
 
-    print("*** tb_lineno:", exc_traceback.tb_lineno)
-
+    print("                        ")
+    print("*** traceback_num_linea:", exc_traceback.tb_lineno)
+    print("                        ")
